816f0d9d1cb3151ce930865fd64dcb67143ac58b
shivam <shivammishra0000024@gmail.com>
2026-08-18 18:58:52 +0530

    Time: 7 ms (64.95%), Space: 58.8 MB (97.9%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,40 +1,40 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int[] outdegree=new int[graph.length];
-        for(int i=0;i<outdegree.length;i++){
-            outdegree[i]=graph[i].length;
+        int[] visited=new int[graph.length];
+        int[] pathVisited=new int[graph.length];
+        int check[]=new int[graph.length];
+        for(int i=0;i<graph.length;i++){
+            if(visited[i]==0){
+                dfs(i,visited,graph,pathVisited,check);
+            }
         }
         List<Integer> list=new ArrayList<>();
-        HashMap<Integer,List<Integer>> map=new HashMap<>();
-       
-        for(int i=0;i<graph.length;i++){
-            for(int j=0;j<graph[i].length;j++){
-                if(!map.containsKey(graph[i][j])){
-                    map.put(graph[i][j],new ArrayList<>());
-                }
-                map.get(graph[i][j]).add(i);
-            }
-            
-        }
-       Queue<Integer> q=new LinkedList<>();
-        for(int i=0;i<outdegree.length;i++){
-            if(outdegree[i]==0){
-               q.offer(i);
-            }
-        }
-        while(!q.isEmpty()){
-            int node=q.poll();
-            list.add(node);
-            for(Integer next:map.getOrDefault(node,new ArrayList<>())){
-                outdegree[next]--;
-                if(outdegree[next]==0){
-                    q.offer(next);
-                }
+        for(int i=0;i<check.length;i++){
+            if(check[i]==1){
+                list.add(i);
             }
         }
         Collections.sort(list);
         return list;
-        
-        
+
+    }
+    public boolean dfs(int node,int[] visited,int[][] graph,int[] pathVisited,int[] check){
+        visited[node]=1;
+        pathVisited[node]=1;
+        check[node]=0;
+        for(Integer it:graph[node]){
+            if(visited[it]==0){
+                if(dfs(it,visited,graph,pathVisited,check)){
+                    return true;
+                }
+            }else{
+                if(pathVisited[it]==1){
+                    return true;
+                }
+            }
+        }
+        check[node]=1;
+        pathVisited[node]=0;
+        return false;
     }
 }

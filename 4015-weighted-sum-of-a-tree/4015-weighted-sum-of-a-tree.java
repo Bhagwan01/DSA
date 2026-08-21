b1be89a6a62a7e95103cd51ef999cd759e6b245e
shivam <shivammishra0000024@gmail.com>
2026-08-21 18:11:09 +0530

    Time: 41 ms (74.99%), Space: 158.6 MB (56.35%) - LeetHub

--- 4015-weighted-sum-of-a-tree/4015-weighted-sum-of-a-tree.java
+++ 4015-weighted-sum-of-a-tree/4015-weighted-sum-of-a-tree.java
@@ -1,55 +1,35 @@
 class Solution {
     public long weightedSum(int[] parent, int[] nums) {
-        List<List<Integer>> adj=new ArrayList<>();
-        
+        List<Integer>[] adj=new ArrayList[parent.length];
         int height=0;
         for(int i=0;i<parent.length;i++){
-            adj.add(new ArrayList<>());
+           adj[i]=new ArrayList<>();
         }
-        for(int i=0;i<parent.length;i++){
-            if(parent[i]==-1){
-                continue;
-            }else{
-                adj.get(parent[i]).add(i);
-            }
+        for(int i=1;i<parent.length;i++){
+             adj[parent[i]].add(i);
+            
         }
         
         long ans=0;
-        int[] visited=new int[parent.length];
-        HashMap<Integer,Integer> map=new HashMap<>();
-        Queue<Pair> q=new LinkedList<>();
-        q.offer(new Pair(0,1));
-        visited[0]=1;
+        int[] depth=new int[parent.length];
+        Queue<Integer> q=new ArrayDeque<>();
+        q.offer(0);
+        depth[0]=1;
         while(!q.isEmpty()){
-            Pair p=q.poll();
-            int curr=p.node;
-            int depth=p.d;
-            map.put(curr,depth);
-            height=Math.max(height,depth);
-            // ans=ans+(nums[curr]*(height-depth+1));
-            //System.out.println(curr+" "+depth+" "+ans);
-            for(Integer it:adj.get(curr)){
-                if(visited[it]==0){
-                    visited[it]=1;
-                    q.offer(new Pair(it,depth+1));
-                }
+            int curr=q.poll();
+            int dep=depth[curr];
+            height=Math.max(height,dep);
+            for(Integer it:adj[curr]){
+                    depth[it]=dep+1;
+                    q.offer(it);
             }
 
         }
         for(int i=0;i<nums.length;i++){
-            int dep=map.get(i);
-            ans=ans+(long)(nums[i])*(height-dep+1);
+            ans=ans+(long)(nums[i])*(height-depth[i]+1);
         }
         return ans;
 
         
-    }
-}
-class Pair{
-    int node;
-    int d;
-    Pair(int a,int b){
-        this.node=a;
-        this.d=b;
     }
 }

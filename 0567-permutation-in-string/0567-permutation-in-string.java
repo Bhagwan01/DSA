6369a78e4bd1c97c5053e681577be7a85f21272b
shivam <shivammishra0000024@gmail.com>
2026-08-28 21:43:30 +0530

    Time: 1 ms (100%), Space: 44 MB (73.79%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,38 +1,26 @@
 class Solution {
-    public boolean checkInclusion(String s1, String s2) {
-        HashMap<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<s1.length();i++){
-            char ch=s1.charAt(i);
-            if(map.containsKey(ch)){
-                int val=map.get(ch);
-                map.put(ch,val+1);
-            }else{
-                map.put(ch,1);
-            }
-        }
-        char[] c2=s1.toCharArray();
-        Arrays.sort(c2);
-        int len=s1.length();
-        int i=0;
-        boolean ans=false;
-        while(i<=(s2.length()-s1.length())){
-            char ch=s2.charAt(i);
-            System.out.println(ch);
-            if(map.containsKey(ch)){
-               int j=i+len-1;
-               String temp=s2.substring(i,j+1);
-               
-               char[] c1=temp.toCharArray();
-               Arrays.sort(c1);
-               ans=Arrays.equals(c1,c2);
-            }
-            if(ans==true){
-                break;
-            }
-            i++;
-        }
-        return ans;
-
+    static{
+        for(int i = 0; i<500; i++)
+            checkInclusion("aa","aa");
     }
-    
+    public static boolean checkInclusion(String s1, String s2) {
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            char ch = s1.charAt(i);
+            arr1[ch - 'a']++;
+        }
+        int low = 0;
+        for(int high = 0; high < s2.length(); high++){
+            arr2[s2.charAt(high)-'a']++;
+            if(high -low +1 > s1.length()){
+                arr2[s2.charAt(low)-'a']--;
+                low++;
+            }
+            if(high -low +1 == s1.length() && Arrays.equals(arr1,arr2)){
+                return true;
+            }
+        }
+        return false;
+    }
 }

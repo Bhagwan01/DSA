827daeb8115ff5fad28a8ccfda7b844620255ac5
shivam <shivammishra0000024@gmail.com>
2026-08-29 16:51:45 +0530

    Time: 2 ms (93.45%), Space: 72.1 MB (42.7%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -4,7 +4,7 @@ class Solution {
         ans[nums.length-1]=1;
         for(int i=nums.length-2;i>=0;i--){
             ans[i]=nums[i+1]*ans[i+1];
-            System.out.println(i+" "+ans[i]);
+           // System.out.println(i+" "+ans[i]);
         }
         int temp=1;
         ans[0]=ans[0]*temp;

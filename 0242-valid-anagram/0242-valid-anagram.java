a8a537f08f076968d92eda9fdbe23a3e997e07a9
shivam <shivammishra0000024@gmail.com>
2026-08-31 23:18:16 +0530

    Time: 4 ms (83.44%), Space: 44.2 MB (89.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -19,11 +19,7 @@ class Solution {
                 alpha[idx]=alpha[idx]-1;
             }
         }
-        for(int i=0;i<26;i++){
-            if(alpha[i]!=0){
-                return false;
-            }
-        }
+        
         return true;
     }
 }

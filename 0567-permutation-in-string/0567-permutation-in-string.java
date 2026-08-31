bdd76ab53b930ebfd43c444ba2373cf61e3dffa0
shivam <shivammishra0000024@gmail.com>
2026-08-31 23:56:44 +0530

    Time: 8 ms (50.65%), Space: 43.7 MB (93.16%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,26 +1,29 @@
 class Solution {
-    static{
-        for(int i = 0; i<500; i++)
-            checkInclusion("aa","aa");
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()){
+            return false;
     }
-    public static boolean checkInclusion(String s1, String s2) {
-        int[] arr1 = new int[26];
-        int[] arr2 = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            char ch = s1.charAt(i);
-            arr1[ch - 'a']++;
+        int[] freq=new int[26];
+        for(int i=0;i<s1.length();i++){
+            freq[s1.charAt(i)-'a']++;
+            freq[s2.charAt(i)-'a']--;
         }
-        int low = 0;
-        for(int high = 0; high < s2.length(); high++){
-            arr2[s2.charAt(high)-'a']++;
-            if(high -low +1 > s1.length()){
-                arr2[s2.charAt(low)-'a']--;
-                low++;
-            }
-            if(high -low +1 == s1.length() && Arrays.equals(arr1,arr2)){
-                return true;
-            }
+        if(allZeros(freq)) return true;
+        for(int i=s1.length();i<s2.length();i++){
+            // move window
+            freq[s2.charAt(i)-'a']--;
+
+            freq[s2.charAt(i-s1.length())-'a']++;
+            if(allZeros(freq)) return true;
         }
         return false;
+    }
+    public boolean allZeros(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                return false;
+            }
+        }
+        return true;
     }
 }

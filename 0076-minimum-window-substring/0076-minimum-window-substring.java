fa898daf39a9cc2a5274f72937cfbc2931c291c2
shivam <shivammishra0000024@gmail.com>
2026-09-03 17:07:28 +0530

    Time: 3 ms (72.2%), Space: 45.9 MB (63.76%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,47 +1,45 @@
 class Solution {
     public String minWindow(String s, String t) {
-        String ans = "";
-        if (t.length() > s.length()) {
-            return ans;
+        if(t.length()>s.length()){
+        return "";
         }
-        int[] freq = new int[58];
-        for (int i = 0; i < t.length(); i++) {
-            freq[t.charAt(i) - 'A']++;
-            freq[s.charAt(i) - 'A']--;
+        int[] freq=new int[58];
+        for(int i=0;i<t.length();i++){
+            freq[t.charAt(i)-'A']++;
         }
-        int minSize = (int) 1e9;
-        if (allZero(freq, t)) {
-           return s.substring(0, t.length());
+        int matched=0;
+        int minSize=(int)1e9;
+        int minIdx=-1;
+        int j=0;
+        for(int i=0;i<t.length();i++){
+            if(freq[s.charAt(i)-'A']>0){
+                matched++;
+            }
+            freq[s.charAt(i)-'A']--;
         }
-
-        int j = 0;
-        int minIdx = 0;
-        for (int i = t.length(); i < s.length(); i++) {
-            freq[s.charAt(i) - 'A']--;
-            while (allZero(freq, t)) {
-                if (minSize > i - j + 1) {
-                    minSize = i - j + 1;
-                    minIdx = j;
+        if(matched==t.length()){
+            return s.substring(0,t.length());
+        }
+        for(int i=t.length();i<s.length();i++){
+            if(freq[s.charAt(i)-'A']>0){
+                matched++;
+            }
+            freq[s.charAt(i)-'A']--;
+            while(matched==t.length()){
+                if(minSize>i-j+1){
+                    minSize=i-j+1;
+                    minIdx=j;
                 }
-                freq[s.charAt(j) - 'A']++;
+                freq[s.charAt(j)-'A']++;
+                if(freq[s.charAt(j)-'A']>0){
+                    matched--;
+                }
                 j++;
-
             }
         }
-
-        if (minSize == (int) 1e9) {
+        if(minSize==(int)1e9){
             return "";
         }
-        return s.substring(minIdx, minIdx + minSize);
-
-    }
-
-    public boolean allZero(int[] arr, String t) {
-        for (int i = 0; i < t.length(); i++) {
-            if (arr[t.charAt(i) - 'A'] > 0) {
-                return false;
-            }
-        }
-        return true;
+        return s.substring(minIdx,minIdx+minSize);
     }
 }

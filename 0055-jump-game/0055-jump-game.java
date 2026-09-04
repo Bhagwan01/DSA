e1c04f86838c6d93aa402a599c4616ca503f0861
shivam <shivammishra0000024@gmail.com>
2026-09-04 16:21:40 +0530

    Time: 2 ms (89.02%), Space: 47.7 MB (80.92%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,36 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int i=0;
-        while(i<nums.length){
-            //System.out.println(nums[i]);
-            if(i==nums.length-1){
-                return true;
-            }
-            if(nums[i]==0){
+        int maxreach=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>maxreach){
                 return false;
             }
-            
-            int j=i+1;
-            int maxIdx=j;
-            while(j<=i+nums[i]){
-                // if(j>=nums.length){
-                //     break;
-                // }
-                if(j==nums.length-1||nums[j]>=nums[i]){
-                    i=j;
-                    break;
-                }
-                if(nums[j]>=nums[maxIdx]){
-                    maxIdx=j;
-                }
-                
-                
-                 if(j==i+nums[i] && j!=nums.length-1 && nums[j]==0){
-                   i=maxIdx;
-                }else if(j==i+nums[i]){
-                    i=j;
-                }
-                j++;
+            maxreach=Math.max(maxreach,i+nums[i]);
+            if(maxreach>=nums.length-1){
+                return true;
             }
         }
         return true;

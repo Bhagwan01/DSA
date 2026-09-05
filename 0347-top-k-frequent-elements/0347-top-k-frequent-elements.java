72ed80baa45c0f36f318756766098c496bfc8e8b
shivam <shivammishra0000024@gmail.com>
2026-09-05 20:19:23 +0530

    Time: 11 ms (96.3%), Space: 51.9 MB (5.12%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,24 +1,33 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        PriorityQueue<int[]> heap=new PriorityQueue<>((x,y)->Integer.compare(y[1],x[1]));
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-        if(map.containsKey(nums[i])){
-            int val=map.get(nums[i]);
-            map.put(nums[i],val+1);
-        }else{
-            map.put(nums[i],1);
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        List<Integer>[] freq = new ArrayList[nums.length + 1];
+
+        for (int i = 0; i <= nums.length; i++) {
+            freq[i] = new ArrayList<>();
         }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                int val = map.get(nums[i]);
+                map.put(nums[i], val + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
         }
-        map.forEach((key,value)->{
-            heap.add(new int[]{key,value});
+        map.forEach((key, value) -> {
+            freq[value].add(key);
         });
-        int ans[]=new int[k];
-        int i=0;
-        while(k>i){
-            int[] curr=heap.poll();
-            ans[i]=curr[0];
-            i++;
+        int ans[] = new int[k];
+        int j = 0;
+        for (int i = freq.length - 1; i >= 0 && j < k; i--) {
+            for (int num : freq[i]) {
+                ans[j++] = num;
+                if (j == k) {
+                    return ans;
+                }
+            }
         }
         return ans;
 

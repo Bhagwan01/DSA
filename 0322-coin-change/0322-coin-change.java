06fec228874cb15e0e3d68116add803df8523416
shivam <shivammishra0000024@gmail.com>
2026-09-05 22:59:41 +0530

    Time: 20 ms (37.56%), Space: 47.2 MB (17.26%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,33 +1,44 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[][] dp=new int[coins.length][amount+1];
-
+         for(int i=0;i<coins.length;i++){
+            dp[i][0]=0;
+        }
         for(int i=0;i<coins.length;i++){
-            for(int j=0;j<=amount;j++){
-                dp[i][j]=-1;
+            for(int j=1;j<=amount;j++){
+                int pick=(int)1e9;
+                int skip=(int)1e9;
+                if(j-coins[i]>=0){
+                    pick=1+dp[i][j-coins[i]];
+                }
+                if(i-1>=0){
+                    skip=dp[i-1][j];
+                }
+                dp[i][j]=Math.min(pick,skip);
+                
             }
         }
-        
-        int ans= helper(coins.length-1,amount,coins,dp);
-        if(ans==(int)1e9){
+        if(dp[coins.length-1][amount]==(int)1e9){
             return -1;
-        }
-        return ans;
-    }
-    public int helper(int idx,int amount,int[] coins,int[][] dp){
-        if(amount==0 ){
-            return 0;
-        }
-    if(amount<0 || idx<0 ){
-        return (int)1e9;
-    }
-    if(dp[idx][amount]!=-1){
-        return dp[idx][amount];
-    }
 
-     int pick=1+helper(idx,amount-coins[idx],coins,dp);
-     int skip=helper(idx-1,amount,coins,dp);
-     return dp[idx][amount]=Math.min(pick,skip);
-
+        }
+        return dp[coins.length-1][amount];
+        
     }
+    // public int helper(int idx,int amount,int[] coins,int[][] dp){
+    //     if(amount==0 ){
+    //         return 0;
+    //     }
+    // if(amount<0 || idx<0 ){
+    //     return (int)1e9;
+    // }
+    // if(dp[idx][amount]!=-1){
+    //     return dp[idx][amount];
+    // }
+
+    //  int pick=1+helper(idx,amount-coins[idx],coins,dp);
+    //  int skip=helper(idx-1,amount,coins,dp);
+    //  return dp[idx][amount]=Math.min(pick,skip);
+
+    // }
 }

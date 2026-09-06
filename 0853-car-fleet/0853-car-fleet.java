f53e0e0af7b77b346c19d45fa29685df726c6584
shivam <shivammishra0000024@gmail.com>
2026-09-07 00:23:59 +0530

    Time: 71 ms (88.38%), Space: 98.4 MB (54.29%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,26 +1,21 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        double[] time=new double[position.length];
-        HashMap<Integer,Integer> map=new HashMap<>();
+        int[][] car=new int[position.length][2];
         for(int i=0;i<position.length;i++){
-            map.put(position[i],speed[i]);
+            car[i][0]=position[i];
+            car[i][1]=speed[i];
         }
-        Arrays.sort(position);
-        for(int i=0;i<speed.length;i++){
-            speed[i]=map.get(position[i]);
-            int dist=target-position[i];
-            time[i]=(double)dist/speed[i];
-        }
-        int fleet=1;
-        double maxtime=time[position.length-1];
-        for(int i=position.length-2;i>=0;i--){
-            //System.out.println(time[i]);
-            if(time[i]>maxtime){
+        Arrays.sort(car,(a,b)->Integer.compare(a[0],b[0]));
+        double maxtime=0.0;
+        int fleet=0;
+        for(int i=speed.length-1;i>=0;i--){
+            double time=(double)(target-car[i][0])/car[i][1];
+            if(time>maxtime){
                 fleet++;
-                maxtime=time[i];
+                maxtime=time;
             }
         }
         return fleet;
-    
+
     }
 }

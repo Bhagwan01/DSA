ba517f281839f54eeb8d0013356d0c1534139062
shivam <shivammishra0000024@gmail.com>
2026-09-07 19:52:16 +0530

    Time: 5 ms (29.32%), Space: 52.3 MB (45.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,26 +1,26 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int[][] visited=new int[grid.length][grid[0].length];
+        boolean[][] visited=new boolean[grid.length][grid[0].length];
         int ans=0;
-        int row[]={-1,0,1,0};
-        int col[]={0,1,0,-1};
+        int[] row={-1,0,1,0};
+        int[] col={0,1,0,-1};
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(visited[i][j]==0 && grid[i][j]=='1'){
+                if(visited[i][j]==false && grid[i][j]=='1'){
+                    dfs(i,j,row,col,visited,grid);
                     ans++;
-                    dfs(i,j,visited,grid,row,col);
                 }
             }
         }
         return ans;
     }
-    public void dfs(int r,int c,int[][] visited,char[][] grid,int[] row,int[] col){
-        visited[r][c]=1;
+    public void dfs(int r,int c,int[] row,int[] col,boolean[][] visited,char[][] grid){
+        visited[r][c]=true;
         for(int i=0;i<4;i++){
-            int tr=r+row[i];
-            int tc=c+col[i];
-            if(tr>=0 && tr<grid.length && tc>=0 && tc<grid[0].length && visited[tr][tc]==0 && grid[tr][tc]=='1'){
-                dfs(tr,tc,visited,grid,row,col);
+            int next_r=r+row[i];
+            int next_c=c+col[i];
+            if(next_r>=0 && next_r<grid.length && next_c>=0 && next_c<grid[0].length && grid[next_r][next_c]=='1' && visited[next_r][next_c]==false){
+                dfs(next_r,next_c,row,col,visited,grid);
             }
         }
     }

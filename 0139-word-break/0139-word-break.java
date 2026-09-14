a80012d39299c0b3322304e1aef63ed919fa76c0
shivam <shivammishra0000024@gmail.com>
2026-09-14 20:56:24 +0530

    Time: 14 ms (6.25%), Space: 46.7 MB (11.53%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -5,23 +5,41 @@ class Solution {
         for(int i=0;i<wordDict.size();i++){
             set.add(wordDict.get(i));
         }
-        return helper(0,0,s,set,dp);
-    }
-    public boolean helper(int st,int end,String s,HashSet<String> dict,Boolean[][] dp){
-        if(end==s.length()-1){
-            if(dict.contains(s.substring(st,end+1))){
-                return true;
+        for(int st=0;st<s.length();st++){
+            if(set.contains(s.substring(st))){
+                dp[st][s.length()-1]=true;
+            }else{
+                dp[st][s.length()-1]=false;
+
             }
-            return false;
         }
-        if(dp[st][end]!=null){
-            return dp[st][end];
+        for(int st=s.length()-1;st>=0;st--){
+            for(int end=s.length()-2;end>=0;end--){
+                boolean skip=false;
+                boolean take=dp[st][end+1];
+                if(end>=st && set.contains(s.substring(st,end+1))){
+                    skip=dp[end+1][end+1];
+                }
+                dp[st][end]=take||skip;
+            }
         }
-        boolean notPick=false;
-        boolean pick=helper(st,end+1,s,dict,dp);
-        if(dict.contains(s.substring(st,end+1))){
-            notPick=helper(end+1,end+1,s,dict,dp);
-        }
-        return dp[st][end]=pick || notPick;
+        return dp[0][0];
     }
+    // public boolean helper(int st,int end,String s,HashSet<String> dict,Boolean[][] dp){
+    //     if(end==s.length()-1){
+    //         if(dict.contains(s.substring(st,end+1))){
+    //             return true;
+    //         }
+    //         return false;
+    //     }
+    //     if(dp[st][end]!=null){
+    //         return dp[st][end];
+    //     }
+    //     boolean notPick=false;
+    //     boolean pick=helper(st,end+1,s,dict,dp);
+    //     if(dict.contains(s.substring(st,end+1))){
+    //         notPick=helper(end+1,end+1,s,dict,dp);
+    //     }
+    //     return dp[st][end]=pick || notPick;
+    // }
 }

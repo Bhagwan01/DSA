a4472f4333e6c3f3ef6d387abe005275d4e9328f
shivam <shivammishra0000024@gmail.com>
2026-09-18 00:00:52 +0530

    Time: 2 ms (87.68%), Space: 44 MB (84%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,60 +1,51 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int ans = 0;
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
-        int fresh=0;
-        int rotten=0;
-        Queue<Pair> q = new LinkedList<>();
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 2) {
+        boolean visited[][]=new boolean[grid.length][grid[0].length];
+        Queue<int[]> q=new ArrayDeque<>();
+        int count=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==2){
                     visited[i][j]=true;
-                    q.offer(new Pair(i,j));
-                    
+                    q.offer(new int[]{i,j});
                 }
                 if(grid[i][j]==1){
-                    fresh++;
+                    count++;
                 }
             }
         }
-        if(fresh==0){
+        int time=0;
+        int rot=0;
+        int row[]={-1,0,1,0};
+        int col[]={0,1,0,-1};
+        if(count==0){
             return 0;
         }
-        int[] row = {-1,0,1,0};
-        int[] col = {0,1,0,-1};
-        
-        while (!q.isEmpty()) {
-            int size=q.size();
-            for (int z = 0; z < size; z++) {
-                Pair p = q.poll();
-                int i = p.row;
-                int j = p.col;
-                for (int k = 0; k < 4; k++) {
-                   int t1 = i + row[k];
-                    int t2 = j + col[k];
-                    if (t1 >= 0 && t1 < grid.length && t2 >= 0 && t2 < grid[0].length && visited[t1][t2] == false && grid[t1][t2]==1) {
-                        visited[t1][t2] = true;
-                        rotten++;
-                        q.offer(new Pair(t1, t2));
-                    }
-                }
+        while(!q.isEmpty()){
+          int size=q.size();
+          for(int j=0;j<size;j++){
+            int[] curr=q.poll();
+          int r=curr[0];
+          int c=curr[1];
+          
+          for(int i=0;i<4;i++){
+            int new_r=r+row[i];
+            int new_c=c+col[i];
+            if(new_r>=0 && new_r<grid.length && new_c>=0 && new_c<grid[0].length && visited[new_r][new_c]==false&&grid[new_r][new_c]==1){
+                visited[new_r][new_c]=true;
+                rot++;
+                q.offer(new int[]{new_r,new_c});
+                
             }
-            ans++;
+          }
+          }
+          time++;
         }
-        if(rotten!=fresh){
+        
+        if(rot!=count){
             return -1;
         }
-        return ans-1;
+        return time-1;
 
-    }
-}
-
-class Pair {
-    int row;
-    int col;
-
-    Pair(int a, int b) {
-        this.row = a;
-        this.col = b;
     }
 }
